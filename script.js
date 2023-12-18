b7f48a65bf5b3de34be63d6d00d9eea6e51b5739
sandranymark <sandra@nymark.nu>
2023-12-18 14:28:12 +0100

    SplitNota

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 // G-Version - Split the Nota
 // Skriv din pseudokod innanför nedanstående kommentarsblock
+
+
+
 /*
 
+FUNCTION SplitNota(summa, antal vänner, dricks):
+    //Beräkna hur mycket var och en ska betala inklusive dricks
+     totalsumma =  summa + (summa * dricksProcent)
 
+    //Beräkna hur mycket var och en av vännerna ska betala
+    beloppPerVän = totalSumma / antalVänner
+
+    //Retunera det belopp var och en av vännerna ska betala
+    returnera beloppPerVän
+END FUNCTION
+
+//SJÄLVA PROGRAMMET
+PRINT "ange notans summa:"
+läs in summa från användaren
+
+IF summa < 0
+    THEN PRINT "Nu blev det J*vligtfel, försök igen!"
+
+PRINT "ange antalet vänner som ska dela på notan:"
+läs in antalVänner från användaren
+
+PRINT "ange hur mycket dricks ni vill ge i decimalform (t.ex. 0.10 för 10%):"
+läs in drcksProcent från användaren
+
+//CALCULATE SplitNota-funktionen och skriv ut reslutatet
+resultat = SplitNota(summa, antalVänner, dricksProcent)
+PRINT "Var och en av vännerna ska betala: " + resultat + "kr"
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // VG-Version - Lewis Carroll Word Puzzle
 // Skriv din pseudokod innanför nedanstående kommentarsblock
